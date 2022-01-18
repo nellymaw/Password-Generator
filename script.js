bde9a070355b237bc6934d46470bd8b10854b6c3
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2022-01-18 15:43:48 +0000

    add docstring to JS file

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!£$%^&*()_+=-#@?><";
-
+/**
+ * Repeats generateChar function, creating a single random
+ * character, for the same amount of times as the password length value
+ */
 function generatePassword(){
     let len = lenBoxElement.value;
 
@@ -36,22 +39,35 @@ function generatePassword(){
     }
     pwElement.innerText = password;
 }
-
+/**
+ * returns random character from uppercase string.
+ */
 function getUppercase() {
     return uppercase[Math.floor(Math.random()*uppercase.length)]
 }
-
+/**
+ * returns random character from lowercase string.
+ */
 function getLowercase() {
     return lowercase[Math.floor(Math.random()*lowercase.length)]
 }
-
+/**
+ * returns random character from numbers string.
+ */
 function getNumber() {
     return numbers[Math.floor(Math.random()*numbers.length)]
 }
-
+/**
+ * returns random character from symbols string.
+ */
 function getSymbols(){
     return symbols[Math.floor(Math.random()*symbols.length)]
 }
+
+/**
+ * Checks which of the criteria boxes are checked, put them inside an array
+ * and returns an random value, unless no criteria is checked, if so, returns empty.
+ */
 
 function generateChar() {
     let chars = [];
@@ -72,14 +88,20 @@ function generateChar() {
     }
     return chars[Math.floor(Math.random()*chars.length)]
 }
-
+//Event Listener for when the Generate button is clicked.
 generateBtnElement.addEventListener('click', generatePassword);
 
+
+//event listener for when the password box is clicked and the text
+//inside is copied to the user's clipboard.
 copyElement.addEventListener('click',copyClipboard);
 pwElement.addEventListener('click',copyClipboard);
 
+
+/**
+ * Function to copy the password to the clipboard.
+ */
 function copyClipboard() {
     var copyText = document.getElementById('pw');
-
     navigator.clipboard.writeText(copyText.innerText);
 }
